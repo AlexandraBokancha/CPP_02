bfc5d61d3c5ba187d71c782d7ed24b270254080d
Alex <bokanchaalexandra@gmail.com>
2024-09-10 19:27:50 +0200

    fixing conversion problem

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,54 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed( void ) : _num(0){
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed( const int intNum ){
+    std::cout << "Int constructor called" << std::endl;
+    this->_num = intNum << _fractionalBits;
+}
+
+Fixed::Fixed( const float floatNum ){
+    std::cout << "Float constructor called" << std::endl;
+    this->_num = floatNum * (1 << _fractionalBits);
+}
+
+Fixed::Fixed(const Fixed& src){ 
+    std::cout << "Copy constructor called" << std::endl;
+    *this = src;
+}
+
+
+Fixed &Fixed::operator=(const Fixed& newFixed){
+    std::cout << "Copy assignement operator called" << std::endl;
+    if (this != &newFixed)
+        this->_num = newFixed.getRawBits();
+    return *this;
+}
+
+int Fixed::getRawBits(void) const {
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->_num;
+}
+
+void Fixed::setRawBits(int const raw){
+    this->_num = raw;
+}
+
+float Fixed::toFloat(void) const {
+    return (float)this->_num / (1 << _fractionalBits);
+}
+
+int Fixed::toInt(void) const {
+    return (int)this->_num / (1 << _fractionalBits);
+}
+
+Fixed::~Fixed(){
+    std::cout << "Destructor called" << std::endl;
+}
+
+std::ostream & operator<<(std::ostream& o, Fixed const & newFixed){
+    o << newFixed.toFloat();
+    return (o);
+}
